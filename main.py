1476a55eb3472046aea9b47433a9cbab6fc682d7
artem4432109-sys <artem4432109@gmail.com>
2026-03-23 00:41:08 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Главный файл для сборки APK - просто импортирует android_hockey.py
+"""
+
+from android_hockey import AirHockeyApp
+
+if __name__ == '__main__':
+    AirHockeyApp().run()
